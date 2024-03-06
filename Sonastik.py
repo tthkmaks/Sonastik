103dddeedfa4095669be449b14fbeb50216583d7
tthmaks <tthkmaks@gmail.com>
2024-03-06 12:58:33 +0200

    a

--- Sonastik.py
+++ Sonastik.py
@@ -5,7 +5,6 @@ def loe_failist(fail: str) -> list:
         jarjend.append(rida.strip())
     f.close()
     return jarjend
-
 def kirjuta_failist(fail: str, jarjend=[]):
     for i in range(7):
         jarjend.append(input(f"{i + 1}. sona: "))
@@ -13,42 +12,30 @@ def kirjuta_failist(fail: str, jarjend=[]):
     for element in jarjend:
         f.write(element + '\n')
     f.close()
-
 def tõlgi(sõnastik, sõna, keel1, keel2):
     if sõna in sõnastik[keel1]:
         indeks = sõnastik[keel1].index(sõna)
         return sõnastik[keel2][indeks]
     else:
-        print(f"{sõna} puudub sõnastikus. Kas soovite selle lisada?")
-        vastus = input("Jah (j) / Ei (e): ").lower()
-        if vastus == 'j':
-            lisa_sõna(sõnastik, sõna, keel1, keel2)
-            return tõlgi(sõnastik, sõna, keel1, keel2)
-        else:
-            return "Sõna ei leitud."
-
-def lisa_sõna(sõnastik, sõna, keel1, keel2):
-    uus_sõna = input(f"Sisestage tõlge sõnale '{sõna}' {keel2}-keelde: ")
-    sõnastik[keel1].append(sõna)
-    sõnastik[keel2].append(uus_sõna)
-    print(f"Sõna '{sõna}' lisatud sõnastikku.")
-
-rus = loe_failist("rus.txt")
-est = loe_failist("est.txt")
-
+        print(f"{sõna} Нет слово!!!")       
 def est_to_rus():
     sõna = input("Введите слово на эстонском: ")
     tõlge = tõlgi({"est": est, "rus": rus}, sõna, "est", "rus")
     print(f"Перевод на русский: {tõlge}")
-
 def rus_to_est():
     sõna = input("Введите слово на русском: ")
     tõlge = tõlgi({"est": est, "rus": rus}, sõna, "rus", "est")
     print(f"Перевод на эстонский: {tõlge}")
+def kontrolli_znaniya(sõnastik, keel1, keel2):
+    pravilno = 0
+    vopros = 0
+procent= pravilno / vopros * 100
+print(f"Результат: {procent}%")
 
+rus = loe_failist("rus.txt")
+est = loe_failist("est.txt")
 print("1.Перевести с эстонского на русский")
 print("2.Перевести с русского на эстонский")
-
 
 while True:
     valik = input("выбор (1/2): ")
@@ -56,6 +43,7 @@ while True:
         est_to_rus()
     elif valik == '2':
         rus_to_est()
+
 
 
 
